2ff8799a7dde8fd2c1bce53b346e40d470fc5521
YayL <jonasbeast9@gmail.com>
2024-12-19 17:42:28 +0100

    initial commit

---
+++ src/main.c
@@ -0,0 +1,6 @@
+#define WINDOW_HEIGHT 720
+#define WINDOW_WIDTH 1920
+
+int main() {
+
+}
---
+++ start.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+cd ./build
+if [ "$1" == "c" ] ; then  # recompile everything
+	rm -rf CMakeFiles
+	rm CMakeCache.txt
+	cmake -D CMAKE_BUILD_TYPE=Debug ..
+	make
+    exit
+fi
+
+set -e
+make
+set +e
+cd ..
+rm coredump.* &> /dev/null
+echo "-------------------------------------------------"
+set -e
+
+
+if [ "$1" == "perf" ] ; then  # perf test
+    rm callgrind.* &> /dev/null # rm callgrind.* > /dev/null 2>&1
+    shift 1
+    valgrind --tool=callgrind --collect-systime=usec ./build/compiler "$@"
+    qcachegrind
+elif [ "$1" == "mem" ] ; then # memory test
+    shift 1
+    valgrind ./build/compiler "$@"
+else
+	./build/compiler "$@"
+fi
